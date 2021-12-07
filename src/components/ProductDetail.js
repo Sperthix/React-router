b6abd178deafd60097d38a2467a8b2cc33e41a77
Sperthix <matusbuci@gmail.com>
2021-12-07 17:57:14 +0100

    using switch nad exact for configurating routes

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,7 +1,10 @@
+import { useParams } from "react-router-dom";
+
 const ProductDetail = () => {
+  const params = useParams();
   return (
     <section>
-      <h1>Product detail.</h1>
+      <h1>{params.productId}</h1>
     </section>
   );
 };
